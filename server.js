0a8a406799b0abc754f36408902ed0c5e8f867fd
Alex zarzitski <alex.zarzitski@gmail.com>
2018-01-07 22:27:35 +0100

    Test ping

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var morgan = require('morgan'); // Charge le middleware de logging
+var logger = require('log4js').getLogger('Server');
+var bodyParser = require('body-parser');
+var app = express();
+
+// config
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan('combined')); // Active le middleware de logging
+
+app.use(express.static(__dirname + '/public')); // Indique que le dossier /public contient des fichiers statiques (middleware chargé de base)
+
+logger.info('server start');
+
+app.get('/', function(req, res){
+    res.redirect('/ping');
+});
+
+app.get('/ping', function(req, res){
+    res.send('pong');
+});
+
+app.listen(process.env.PORT||1313);
