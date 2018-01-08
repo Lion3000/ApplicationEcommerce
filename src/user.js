e160efa6a595bcfd23e93c2b4bb4545d3f394b2b
Camélia Benhmida <benhmida.camelia@gmail.com>
2018-01-08 13:22:45 +0100

    test entity2

---
+++ src/user.js
@@ -0,0 +1,13 @@
+class User {
+
+  constructor(name, firstName) {
+    this.name = name;
+    this.firstName = firstName;
+  }
+
+  sayHi() {
+    alert(this.name + this.firstName);
+  }
+}
+module.exports = User;
+
