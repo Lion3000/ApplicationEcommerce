22e75bd15a93e620865e1f44428a814991088b59
Camélia Benhmida <benhmida.camelia@gmail.com>
2018-01-11 11:44:40 +0100

    test entity with sequelize prof model

---
+++ src/model/user.js
@@ -0,0 +1,18 @@
+var db = require(./../../databaseConnexion.js);
+var sequelize = require(sequelize);
+
+const User = sequelize.define('user', {
+  firstName: {
+    type: Sequelize.STRING
+  },
+  lastName: {
+    type: Sequelize.STRING
+  }
+});
+
+User.sync({force: false}).then(() => {
+  // Table created
+});
+
+module.exports = User;
+
