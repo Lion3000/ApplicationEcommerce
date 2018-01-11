a49115231301a2ecbdab9fa188de85dbe6e32684
Camélia Benhmida <benhmida.camelia@gmail.com>
2018-01-11 09:09:11 +0100

    test entity 2

--- src/user.js
+++ src/user.js
@@ -3,11 +3,11 @@ class User {
   constructor(nom = "", prenom = "") {
     this.nom = nom;
     this.prenom = prenom;
-    this.email = email;
-	this.mdp = mdp;
-	this.adresse = adresse;
-	this.complementAdresse = complementAdresse;
-	this.dateNaissance = dateNaissance;
+    // this.email = email;
+	// this.mdp = mdp;
+	// this.adresse = adresse;
+	// this.complementAdresse = complementAdresse;
+	// this.dateNaissance = dateNaissance;
   }
 };
 module.exports = User;
