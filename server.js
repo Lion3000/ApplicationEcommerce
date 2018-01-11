7300de09abacb4a4331a5011bfe7d53fdacde6c8
Camélia Benhmida <benhmida.camelia@gmail.com>
2018-01-11 13:26:51 +0100

    test entity with sequelize prof model suite

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/', function(req, res){
 app.get('/ping', function(req, res){
 	
 var sequelize = require("./src/databaseConnexion.js");
-//var sequelize = require(sequelize);
+var Sequelize = require(Sequelize);
 	
 sequelize.authenticate().then(() => {
     console.log('Connection has been established successfully.');
