3da44c44a792a2d5d11e1bc212fec494d1b69d33
Camélia Benhmida <benhmida.camelia@gmail.com>
2018-01-11 17:48:01 +0100

    ajout entities

--- src/entities/user.js
+++ src/entities/user.js
@@ -4,18 +4,39 @@ Date : 11/01/2018
 ==============================================================*/
 const Sequelize = require('sequelize');
 var sequelize = require("../sequelize.js");
+var Panier = require("panier.js");
 var db = sequelize.connection();
 
 const User = db.define('user', {
-  firstName: {
+  nom: {
     type: Sequelize.STRING
   },
-  lastName: {
+  prenom: {
     type: Sequelize.STRING
+  },
+  email: {
+    type: Sequelize.STRING
+  },
+  mdp: {
+    type: Sequelize.STRING
+  },
+  dateNaissance: {
+    type: Sequelize.DATE
+  },
+  adresse: {
+    type: Sequelize.STRING
+  },
+  complementAdresse: {
+    type: Sequelize.STRING
+  },
+  codePostal: {
+    type: Sequelize.STRING
+  },
+  isAdmin: {
+    type: Sequelize.BOOLEAN
   }
 });
-
+Panier.hasOne(User); // ajoute dans user l'id du Panier
 User.sync({force: false}).then(() => {});
 
 module.exports = User;
-
