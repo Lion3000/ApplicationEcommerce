edc01606f742f18d71a5b38cc56f8f9ff492a31a
Camélia Benhmida <benhmida.camelia@gmail.com>
2018-01-11 09:05:54 +0100

    ajout uml

--- src/user.js
+++ src/user.js
@@ -1,8 +1,13 @@
 class User {
 
-  constructor(name, firstName) {
-    this.name = name;
-    this.firstName = firstName;
+  constructor(nom, prenom) {
+    this.nom = nom;
+    this.prenom = prenom;
+    this.email = email;
+	this.mdp = mdp;
+	this.adresse = adresse;
+	this.complementAdresse = complementAdresse;
+	this.dateNaissance = dateNaissance;
   }
 };
 module.exports = User;
