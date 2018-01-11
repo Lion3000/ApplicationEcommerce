eebe5db74fcb30847f8c4ce8a3d0906aadc0ec34
Alex zarzitski <alex.zarzitski@gmail.com>
2018-01-11 17:30:56 +0100

    fin du test de la session

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var morgan = require('morgan'); // Charge le middleware de logging
 var logger = require('log4js').getLogger('Server');
 var bodyParser = require('body-parser');
 var routes = require('./src/routes');
+var session = require('express-session');
 var co = require('co');
 var app = express();
 
@@ -14,8 +15,8 @@ var app = express();
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/src/views');
 app.use(bodyParser.urlencoded({ extended: false }));
-//bodyParser.json()
 app.use(morgan('combined')); // Active le middleware de logging
+app.use(session({ secret: 'd8z*fz8er7', cookie: { maxAge: 60000 }})); // maxAge = expire time
 
 app.use(express.static(__dirname + '/public')); // Indique que le dossier /public contient des fichiers statiques (middleware chargé de base)
 
--- src/routes.js
+++ src/routes.js
@@ -18,7 +18,18 @@ module.exports = {
     });
 	
 	app.get('/home', function(req, res){
-        res.render('home');
+		if (req.session.views) {
+			req.session.views++;
+			res.setHeader('Content-Type', 'text/html');
+			res.write('<p>views: ' + req.session.views + '</p>');
+			res.write('<p>expires in: ' + (req.session.cookie.maxAge / 1000) + 's</p>');
+			res.end();
+		} 
+		else {
+			req.session.views = 1;
+			res.end('welcome to the session demo. refresh!');
+		}
+        //res.render('home');
     });
 
     app.post('/signup', function (req, res) {
