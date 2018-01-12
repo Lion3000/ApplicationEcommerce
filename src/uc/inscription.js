028662f3b2f48cce1de53ec0ffde08fe6e3fb7b7
Alex zarzitski <alex.zarzitski@gmail.com>
2018-01-12 11:36:29 +0100

    test2 co user

--- src/uc/inscription.js
+++ src/uc/inscription.js
@@ -8,16 +8,17 @@ var User = require(appRoot + "/src/entities/user.js");
 module.exports = {
 
   doIt: function * (app) {
-  	var user = yield User.create({ nom : "test", prenom:"test"});
-  	user.nom = "truc";
-  	user.save();
-  	user2 = yield User.findById(user.id);
-  	console.log("ICI<-------------------------------------------->" + user2.id );
+
 
   	/*user2.destroy();*/
 
     app.get('/signup', function(req, res){
 			res.render('signup');
+      var user = yield User.create({ nom : "test", prenom:"test"});
+      user.nom = "truc";
+      user.save();
+      user2 = yield User.findById(user.id);
+      console.log("ICI<-------------------------------------------->" + user2.id );
     });
 
     app.post('/signup', function (req, res) {
