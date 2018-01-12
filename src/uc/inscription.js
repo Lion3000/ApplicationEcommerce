13ae2ea9742de7562aad6ba3b22240a7c8c66a96
Alex zarzitski <alex.zarzitski@gmail.com>
2018-01-12 19:57:10 +0100

    test inscription

--- src/uc/inscription.js
+++ src/uc/inscription.js
@@ -12,24 +12,74 @@ var UcInscription = {
   doIt: function(app) {
 
     app.get('/signup', function(req, res){
-      co(UcInscription.test(res));
+      res.render('signup');
     });
 
     app.post('/signup', function (req, res) {
-        res.send(req.param('email'));
+      var errors = [];
+      var user = { nom : "", prenom : "", email : "", mdp : "", dateNaissance : "", adresse : "", complementAdresse : "", codePostal : "", isAdmin : false};
+      UcInscription.getUserFromForm(req, user, errors);
+      if(errors.length < 1)
+        co(UcInscription.addNewUser(res, user, errors));
+      else
+        res.render('signup', {user : user, errors : errors});
     });
 
   },
 
-  test: function * (res) {
+  getUserFromForm: function(req, user, errors) {
+    if(req.param('name') != "")
+      user.nom = req.param('name');
+    else
+      errors.push("nom");
+
+    if(req.param('surname') != "")
+      user.prenom = req.param('surname');
+    else
+      errors.push("prenom");
+
+    if(req.param('password') != "")
+      user.mdp = req.param('password');
+    else
+      errors.push("mdp");
+
+    if(req.param('birthday') != "")
+      user.dateNaissance = req.param('birthday');
+    else
+      errors.push("dateNaissance");
+
+    if(req.param('address') != "")
+      user.adresse = req.param('address');
+    else
+      errors.push("adresse");
+
+    if(req.param('address_supplement') != "")
+      user.complementAdresse = req.param('address_supplement');
+    else
+      errors.push("complementAdresse");
+
+    if(req.param('postal_code') != "")
+      user.codePostal = req.param('postal_code');
+    else
+      errors.push("codePostal");
+  },
+
+  addNewUser: function * (res, user) {
     var User = require(appRoot + "/src/entities/user.js");
-    res.render('signup');
-    var user = yield User.create({ nom : "test", prenom:"test"});
-    user.nom = "truc";
+    var userC = yield User.create(user);
+    /*user.nom = "truc";
     user.save();
     var user2 = yield User.findById(user.id);
     console.log("ICI<----------------->" + user2.id );
-    user2.destroy();
+    user2.destroy();*/
+
+    if(errors.length < 1)
+      req.session.idUser = userC.id;
+      res.redirect('/home');
+    else{
+      res.render('signup', {user : user, errors : errors});
+    }
+
   }
 
 }
