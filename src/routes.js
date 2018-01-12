984ba8e9798cabca9507cfce117efe3bbd1f9b88
Alex zarzitski <alex.zarzitski@gmail.com>
2018-01-12 20:15:00 +0100

    test inscription

--- src/routes.js
+++ src/routes.js
@@ -9,29 +9,18 @@ var co = require('co');
 module.exports = {
   start: function(app) {
 
+	  UcInscription.doIt(app);
+
     app.get('/', function(req, res){
-        res.redirect('/login');
+      res.redirect('/login');
     });
 
     app.get('/login', function(req, res){
-        res.render('login');
+      res.render('login');
     });
 
-	UcInscription.doIt(app);
-
-	app.get('/home', function(req, res){
-		if (req.session.views) {
-			req.session.views++;
-			res.setHeader('Content-Type', 'text/html');
-			res.write('<p>views: ' + req.session.views + '</p>');
-			res.write('<p>expires in: ' + (req.session.cookie.maxAge / 1000) + 's</p>');
-			res.end();
-		}
-		else {
-			req.session.views = 1;
-			res.end('welcome to the session demo. refresh!');
-		}
-        //res.render('home');
+	  app.get('/home', function(req, res){
+      res.render('home');
     });
 
   }
