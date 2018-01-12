68ea106d0bbd004bb72ff2a36a12714203cea9dc
Alex zarzitski <alex.zarzitski@gmail.com>
2018-01-12 14:33:09 +0100

    test2 co user

--- src/uc/inscription.js
+++ src/uc/inscription.js
@@ -6,9 +6,16 @@ Objet : UcInscription
 var appRoot = require('path').dirname(require.main.filename);
 var co = require('co');
 var User = require(appRoot + "/src/entities/user.js");
-module.exports = {
 
-  doIt: function(app) {
+var Rectangle = class {
+  constructor(hauteur, largeur) {
+    this.hauteur = hauteur;
+    this.largeur = largeur;
+  }
+};
+module.exports =  class {
+
+  doIt(app) {
 
     app.get('/signup', function(req, res){
       co(function * () {
@@ -21,15 +28,16 @@ module.exports = {
         console.log("ICI<----------------->" + user2.id );
         user2.destroy();
       });
+
     });
 
     app.post('/signup', function (req, res) {
         res.send(req.param('email'));
     });
 
-  },
+  }
 
-  test: function * () {
+  test*() {
     var User = require(appRoot + "/src/entities/user.js");
     res.render('signup');
     var user = yield User.create({ nom : "test", prenom:"test"});
@@ -37,7 +45,7 @@ module.exports = {
     user.save();
     var user2 = yield User.findById(user.id);
     console.log("ICI<----------------->" + user2.id );
-    /*user2.destroy();*/
+    user2.destroy();
   }
 
 }
