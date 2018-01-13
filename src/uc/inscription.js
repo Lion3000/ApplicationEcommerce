fcacef5e2b119f543afd8adb0f4ff5ce345301dc
Alex zarzitski <alex.zarzitski@gmail.com>
2018-01-13 22:22:23 +0100

    nettoyage du code UcInscription

--- src/uc/inscription.js
+++ src/uc/inscription.js
@@ -8,43 +8,41 @@ var User = require(appRoot + "/src/entities/user.js");
 var co = require('co');
 
 var UcInscription = {
-  
+
   doIt: function(app) {
+    app.get('/signup', UcInscription.showForm);
 
-    app.get('/signup', function(req, res){
-      var errors = [];
-      var user = { nom : "", prenom : "", email : "", mdp : "", dateNaissance : "", adresse : "", complementAdresse : "", codePostal : "", isAdmin : false};
-
-      res.render('signup', {user : user, errors : errors});
-    });
-
-	var fn2 = co.wrap(function * (req, res) {
-      var errors = [];
-      var user = { nom : "", prenom : "", email : "", mdp : "", dateNaissance : "", adresse : "", complementAdresse : "", codePostal : "", isAdmin : false};
-
-      UcInscription.getUserFromForm(req, user, errors);
-	  
-	  var result = {ref: -1, error: "test"};
-	  
-	  //var fn = co.wrap(UcInscription.addNewUser);
-	  UcInscription.addNewUser(user, errors, result);
-	  
-	  console.log("ICI5<---------------------->");
-	  
-	  if(result.error != "")
-		  errors.push(result.error);
-	  
-      if(result.ref != -1){
-		req.session.idUser = result.ref;
-		resTest.redirect('/home');
-	  }
-      else
-        res.render('signup', {user : user, errors : errors});
-    });
-	
-	app.post('/signup', fn2);
-
+  	var signup = co.wrap(UcInscription.signup);
+  	app.post('/signup', signup);
   },
+
+  showForm: function (req, res){
+    var errors = [];
+    var user = { nom : "", prenom : "", email : "", mdp : "", dateNaissance : "", adresse : "", complementAdresse : "", codePostal : "", isAdmin : false};
+
+    res.render('signup', {user : user, errors : errors});
+  }
+
+  signup: function * (req, res) {
+    var errors = [];
+    var user = { nom : "", prenom : "", email : "", mdp : "", dateNaissance : "", adresse : "", complementAdresse : "", codePostal : "", isAdmin : false};
+
+    UcInscription.getUserFromForm(req, user, errors);
+
+    var userId = {ref: -1};
+
+    var addNewUser = co.wrap(UcInscription.addNewUser);
+    yield addNewUser(user, errors, result);
+
+    console.log("ICI5<---------------------->");
+
+    if(userId.ref != -1){
+      req.session.userId = userId.ref;
+      resTest.redirect('/home');
+    }
+    else
+      res.render('signup', {user : user, errors : errors});
+  }
 
   getUserFromForm: function(req, user, errors) {
     if(req.param('name') != "")
@@ -88,28 +86,27 @@ var UcInscription = {
       errors.push("codePostal");
   },
 
-  addNewUser: function * (user, errors, result) {
-	result.ref = -1;
-	try{
-		console.log("ICI0<---------------------->");
-		if (errors.length == 0) {
-			var userTmp = yield User.findOne({ where : {email: user.email } });
-			console.log("ICI1<---------------------->");
-			if (userTmp == null) {
-				console.log("ICI2<---------------------->");
-				user = yield User.create(user);
-				result.ref = user.id;
-			}
-			else{
-				result.error = "Email deja utilisé !";
-				console.log("ICI4<---------------------->");
-			}
-		}
-	}
-	catch(e){
-		console.log("ICI3<---------------------->" + e);
-		//errors.push(e + "");
-	}
+  addNewUser: function * (user, errors, userId) {
+  	userId.ref = -1;
+  	try{
+  		console.log("ICI0<---------------------->");
+  		if (errors.length == 0) {
+  			var userTmp = yield User.findOne({ where : {email: user.email } });
+  			console.log("ICI1<---------------------->");
+  			if (userTmp == null) {
+  				console.log("ICI2<---------------------->");
+  				user = yield User.create(user);
+  				userId.ref = user.id;
+  			}
+  			else{
+  				errors.push("Email deja utilisé !");
+  				console.log("ICI4<---------------------->");
+  			}
+  		}
+  	}
+  	catch(e){
+  		errors.push(e + "");
+  	}
   }
 
 }
