afdb99cddcaf197140373b8b34a1ebc2f1e5b0d4
Camélia Benhmida <benhmida.camelia@gmail.com>
2018-01-16 11:47:42 +0100

    produit only

--- src/entities/produit.js
+++ src/entities/produit.js
@@ -4,8 +4,8 @@ Date : 11/01/2018
 ==============================================================*/
 var appRoot = require('path').dirname(require.main.filename);
 const Sequelize = require('sequelize');
-var Categorie = require(appRoot + "/src/entities/categorie.js");
-var ProduitSelectionne = require(appRoot + "/src/entities/produitSelectionne.js");
+//var Categorie = require(appRoot + "/src/entities/categorie.js");
+// var ProduitSelectionne = require(appRoot + "/src/entities/produitSelectionne.js");
 var sequelize = require(appRoot + "/src/sequelize.js");
 var db = sequelize.connection();
 
@@ -27,7 +27,7 @@ const Produit = db.define('produit', {
   }
 });
 //Produit.belongsTo(Categorie,  { onDelete: 'CASCADE' });
-Produit.belongsToMany(ProduitSelectionne, { onDelete: 'CASCADE' });
+//Produit.belongsToMany(ProduitSelectionne, { onDelete: 'CASCADE' });
 
 Produit.sync({force: false}).then(() => {});
 module.exports = Produit;
