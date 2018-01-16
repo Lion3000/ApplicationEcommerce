4a590428a20628ca9c13a40036547b5d5c33fbb9
Camélia Benhmida <benhmida.camelia@gmail.com>
2018-01-16 11:03:19 +0100

    correction associations db require

--- src/entities/panier.js
+++ src/entities/panier.js
@@ -5,6 +5,7 @@ Date : 11/01/2018
 var appRoot = require('path').dirname(require.main.filename);
 const Sequelize = require('sequelize');
 var sequelize = require(appRoot + "/src/sequelize.js");
+var ProduitSelectionne = require(appRoot + "/src/entities/produitSelectionne.js");
 var db = sequelize.connection();
 
 const Panier = db.define('panier', {
