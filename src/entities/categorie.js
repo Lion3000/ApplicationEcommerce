63693d3cc84951860173a492ea37fbcaf5da12b6
Camélia Benhmida <benhmida.camelia@gmail.com>
2018-01-17 13:22:37 +0100

    test sequelize

--- src/entities/categorie.js
+++ src/entities/categorie.js
@@ -34,10 +34,12 @@ const Produit = db.define('produit', {
 });
 
 //Categorie.hasMany(Produit, { onDelete: 'cascade' }); // catérogieId dans produit + getProduits dans catégorie
-Produit.belongsTo(Categorie,  { foreignKeyConstraint: true, onDelete: 'CASCADE' });
+
 Categorie.hasMany(Produit, { foreignKeyConstraint: true, as: 'Produits', onDelete: 'CASCADE' }); // catérogieId dans produit + getProduits dans catégorie
-Produit.sync({force: true}).then(() => {});
+Produit.belongsTo(Categorie,  { foreignKeyConstraint: true, onDelete: 'CASCADE' });
 Categorie.sync({force: true}).then(() => {});
+Produit.sync({force: true}).then(() => {});
+
 
 //db.sync({ force: true });
 module.exports = Categorie;
