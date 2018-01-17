5e50d984a3e2ccdaee2b95b8b0339210bdcf92ad
Camélia Benhmida <benhmida.camelia@gmail.com>
2018-01-18 00:52:44 +0100

    fin

--- src/sequelize.js
+++ src/sequelize.js
@@ -6,8 +6,8 @@ const Sequelize = require('sequelize');
 
 module.exports = {
 	connection: function(app) {
-		//const sequelize = new Sequelize('postgres://bdbiecpfkwqiqa:09f3c26b0e7d04bc9cf0a31f548e6c02b6d0cc4fef583d78fa5438eb44d0c9a7@ec2-54-217-218-80.eu-west-1.compute.amazonaws.com:5432/dbjg84geapj4kl');
-		const sequelize = new Sequelize('postgres://postgres:123456@localhost:5432/postgres');
+		const sequelize = new Sequelize('postgres://bdbiecpfkwqiqa:09f3c26b0e7d04bc9cf0a31f548e6c02b6d0cc4fef583d78fa5438eb44d0c9a7@ec2-54-217-218-80.eu-west-1.compute.amazonaws.com:5432/dbjg84geapj4kl');
+		//const sequelize = new Sequelize('postgres://postgres:123456@localhost:5432/postgres');
 
 		sequelize.authenticate().then(() => {
 			console.log('Connection has been established successfully.');
