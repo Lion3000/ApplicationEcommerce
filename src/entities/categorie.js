616d256a6d738904b110b05b48c96592ee11d6fc
Camélia Benhmida <benhmida.camelia@gmail.com>
2018-01-17 10:56:12 +0100

    test sequelize

--- src/entities/categorie.js
+++ src/entities/categorie.js
@@ -5,7 +5,7 @@ Date : 11/01/2018
 var appRoot = require('path').dirname(require.main.filename);
 const Sequelize = require('sequelize');
 var sequelize = require(appRoot + "/src/sequelize.js");
-//var Produit = require(appRoot + "/src/entities/produit.js");
+var Produit = require(appRoot + "/src/entities/produit.js");
 
 var db = sequelize.connection();
 
